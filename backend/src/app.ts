e6c1e2a685c0cf8992d6f28b173bd911fb02b0fd
Matt Shindala <matt.shindala@gmail.com>
2019-09-01 02:51:55 -0500

    Redirect HTTP requests to HTTPS in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,19 @@ import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import comments from './comments';
 
-// middleware
+// In production, trust X-Forwarded-Proto, X-Forwarded-Host, and
+// X-Forwarded-For headers, and redirect HTTP requests to HTTPS
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy');
+  app.use((req, res, next) => {
+    if (req.secure) {
+      next();
+    } else {
+      res.redirect(301, "https://" + req.headers.host + req.originalUrl);
+    }
+  });
+}
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
